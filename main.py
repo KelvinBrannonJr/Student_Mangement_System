c10c63ed7645bf37acfdcbe10e606b93e6b46282
Kelvin <Kelzjr56@gmail.com>
2023-06-06 10:22:25 -0400

    Connected SQL database and extracted data. Create QTableWidget, emplace db data in its columns and rows

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import sqlite3
 import sys
 
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow, QTableWidget
+    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 
 from PyQt6.QtGui import QAction
 
@@ -23,18 +24,42 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
+        # QTableWidget attributes
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
 
+        # To hide default vertical numbers not associated with SQL database
+        self.table.verticalHeader().setVisible(False)
+
+        # Set a center layout widget to QTableWidget instance
         self.setCentralWidget(self.table)
 
-
     def load_data(self):
-        self.table
+        # Connect SQL database
+        connection = sqlite3.Connection("database.db")
+        results = connection.execute("SELECT * FROM students")
 
+        # Initialize table number to 0
+        self.table.setRowCount(0)
+
+        # Iterate through row numbers
+        for row_number, row_data in enumerate(results):
+
+            # Every index insert a row cell with a row number
+            self.table.insertRow(row_number)
+
+            # Iterate through column numbers
+            for column_number, column_data in enumerate(row_data):
+
+                # Every index of a row number and column number add column data
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(column_data)))
+
+        # Close the database connection
+        connection.close()
 
 app = QApplication(sys.argv)
-age_calculator = MainWindow()
-age_calculator.show()
+student_management_sys = MainWindow()
+student_management_sys.show()
+student_management_sys.load_data()
 sys.exit(app.exec())
